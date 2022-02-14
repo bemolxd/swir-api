f1db774f02c1c9f8870d487bf8195d3394c8f024
bemolx <bartosz.bem@bemideas.pl>
2022-02-14 13:26:00 +0100

    [Server Response] stworzenie bazowego kontrolera z potrzebnymi metodami

---
+++ src/shared/core/BaseController.ts
@@ -0,0 +1,36 @@
+import { Response } from 'express';
+
+interface ResponseBody {
+  message: string;
+  error?: any;
+}
+
+export abstract class BaseController {
+  public static jsonResponse(res: Response, code: number, body: ResponseBody) {
+    return res.status(code).json({ ...body });
+  }
+
+  public created(res: Response) {
+    return res.sendStatus(201);
+  }
+
+  public notFound(res: Response, { message }: ResponseBody) {
+    return BaseController.jsonResponse(res, 404, {
+      message: message ?? 'Not found',
+    });
+  }
+
+  public ok<T>(res: Response, dto?: T) {
+    if (!!dto) {
+      res.type('application/json');
+      return res.status(200).json(dto);
+    }
+
+    return res.sendStatus(200);
+  }
+
+  public fail<T>(res: Response, error: T | string) {
+    const err = typeof error === 'string' ? { message: error } : error;
+    return res.status(500).json(err);
+  }
+}
