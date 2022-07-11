e20bd8a407aa6d514caf9a010be653061bb46f7e
bemolx <bartosz.bem@bemideas.pl>
2022-07-11 15:25:46 +0200

    [AdminOrder] create proper controller logic

--- src/modules/orders/application/useCases/finishOrder/finish-order.controller.ts
+++ src/modules/orders/application/useCases/finishOrder/finish-order.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Param, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { BaseController } from 'shared/core';
+import { AppError, BaseController } from 'shared/core';
 
 import { OrderService } from '../../services';
 import { FinishOrderBodyDto } from './finish-order.dto';
+import { FinishOrderErrors } from './finish-order.errors';
+import { FinishOrderResponse } from './finish-order.use-case';
 
 @Controller()
 export class FinishOrderController extends BaseController {
@@ -16,5 +18,23 @@ export class FinishOrderController extends BaseController {
     @Param('orderId') orderId: string,
     @Body() dto: FinishOrderBodyDto,
     @Res() res: Response,
-  ) {}
+  ) {
+    try {
+      const result: FinishOrderResponse = await this.orderService.finishOrder({
+        orderId,
+        techComment: dto.techComment,
+      });
+
+      if (result instanceof FinishOrderErrors.OrderNotFound) {
+        return this.badRequest(res, result);
+      }
+
+      return this.ok(res);
+    } catch (error) {
+      return this.fail<AppError.UnexpectedError>(
+        res,
+        new AppError.UnexpectedError(error),
+      );
+    }
+  }
 }
