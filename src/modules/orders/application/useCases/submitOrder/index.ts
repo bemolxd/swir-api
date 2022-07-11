cfaaaf868023e74e2d762494cd758d1d32f71859
bemolx <bartosz.bem@bemideas.pl>
2022-07-11 14:07:37 +0200

    [Orders] re-export use-cases for better readability

--- src/modules/orders/application/useCases/submitOrder/index.ts
+++ src/modules/orders/application/useCases/submitOrder/index.ts
@@ -3,3 +3,4 @@ export {
   SubmitOrderResponse,
   SubmitOrderUseCase,
 } from './submit-order.use-case';
+export { SubmitOrderController } from './submit-order.controller';
--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -9,73 +9,16 @@ import { QueryHandlers } from './application/queries/handlers';
 
 import { OrderService } from './application/services';
 
-import {
-  GetOrderUseCase,
-  GetOrderController,
-} from './application/useCases/getOrder';
-import {
-  GetOrdersController,
-  GetOrdersUseCase,
-} from './application/useCases/getOrders';
-import {
-  CreateOrderController,
-  CreateOrderUseCase,
-} from './application/useCases/createOrder';
-import {
-  GetUserOrdersController,
-  GetUserOrdersUseCase,
-} from './application/useCases/getUserOrders';
-import {
-  AddItemElementController,
-  AddItemElementUseCase,
-} from './application/useCases/addItemElement';
-import {
-  RemoveItemElementController,
-  RemoveItemElementUseCase,
-} from './application/useCases/removeItemElement';
-import {
-  DeleteOrderController,
-  DeleteOrderUseCase,
-} from './application/useCases/deleteOrder';
-import { SubmitOrderUseCase } from './application/useCases/submitOrder';
-import { SubmitOrderController } from './application/useCases/submitOrder/submit-order.controller';
-import {
-  RejectOrderController,
-  RejectOrderUseCase,
-} from './application/useCases/rejectOrder';
-import {
-  AcceptOrderController,
-  AcceptOrderUseCase,
-} from './application/useCases/acceptOrder';
+import { OrderControllers, OrderUseCases } from './application/useCases';
 
 @Module({
   imports: [CqrsModule, TypeOrmModule.forFeature([OrderRepository])],
-  controllers: [
-    GetOrdersController,
-    GetOrderController,
-    CreateOrderController,
-    GetUserOrdersController,
-    AddItemElementController,
-    RemoveItemElementController,
-    DeleteOrderController,
-    SubmitOrderController,
-    RejectOrderController,
-    AcceptOrderController,
-  ],
+  controllers: [...OrderControllers],
   providers: [
     OrderService,
     ...QueryHandlers,
     ...CommandHandlers,
-    GetOrdersUseCase,
-    GetOrderUseCase,
-    CreateOrderUseCase,
-    GetUserOrdersUseCase,
-    AddItemElementUseCase,
-    RemoveItemElementUseCase,
-    DeleteOrderUseCase,
-    SubmitOrderUseCase,
-    RejectOrderUseCase,
-    AcceptOrderUseCase,
+    ...OrderUseCases,
   ],
 })
 export class OrdersModule {}
