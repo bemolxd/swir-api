be8cdbb294e10381269eeb7896351e996b757646
Bem Bartosz <bartosz.bem@grupawp.pl>
2022-10-25 22:45:27 +0200

    feat: create docs for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Request, Response } from 'express';
+
+import { UserDto } from 'modules/users/application/dto';
 
 import { AuthenticatedGuard, OAuth2Guard } from './guards';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   @Get('cui')
+  @ApiResponse({ status: 307, description: 'Redirect to CPL PG services' })
   @UseGuards(OAuth2Guard)
   login() {
     return;
@@ -13,17 +18,20 @@ export class AuthController {
 
   @Get('cui/redirect')
   @UseGuards(OAuth2Guard)
+  @ApiResponse({ status: 307, description: 'Redirect to APP default page URL' })
   redirect(@Res() res: Response) {
     res.redirect(process.env.APP_CLIENT_URL);
   }
 
   @Get('me')
+  @ApiOkResponse({ type: UserDto })
   @UseGuards(AuthenticatedGuard)
   protected(@Req() req: Request) {
     return req.user;
   }
 
   @Get('logout')
+  @ApiResponse({ status: 302, description: 'Logout from SWiR and CPL PG' })
   @UseGuards(AuthenticatedGuard)
   logout(@Req() req: Request, @Res() res: Response) {
     req.logout();
